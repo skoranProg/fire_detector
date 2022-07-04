ea834c0752db4900a1aaf85449f5816ce3bb1a93
skoranProg <85221867+skoranProg@users.noreply.github.com>
2022-07-05 01:55:08 +0300

    2.0

--- code/main.py
+++ code/main.py
@@ -1,14 +1,17 @@
-import numpy as np
+import cupy as np
+import numpy.random
 import pandas as pd
 from numpy.random import shuffle
-
+import matplotlib.pyplot as plt
 import nweb
-from nweb import set_hyper_param as shp, NeuronWeb
+from nweb import set_hyper_param as shp
+from nweb import NeuronWeb
 
 
 # functions
 def tts(a: list, b: list, k: float):
     c = list(zip(a, b))
+    shuffle(c)
     return c[:int(k * len(c))], c[int(k * len(c)):]
 
 
@@ -18,41 +21,62 @@ def new_nw(architecture: list[int]):
     return res
 
 
+def random_cost():
+    c = 0
+    for tx, ty in test:
+        c += np.sum(nweb.cost_function(ty, 0.99 * np.random.rand(len(ty)) + 0.001)) / len(ty)
+    return c / len(test)
+
+
 # global variables
 data_path = '../data/coffee.csv'
+saved_nw_path = '../saves'
+input_days = 60
+output_days = 1
+amount_of_test_data = 1 / 6
+costs_to_show = []
 
 # data input
 data = pd.read_csv(data_path)
-low = np.array(data['Low'])
-high = np.array(data['High'])
-close = np.array(data['Close'])
-opn = np.array(data['Open'])
-vol = np.array(data['Volume'])
+low = np.array(data['Low'], dtype=float)
+high = np.array(data['High'], dtype=float)
+close = np.array(data['Close'], dtype=float)
+opn = np.array(data['Open'], dtype=float)
+vol = np.array(data['Volume'], dtype=float)
 
 # data formatting
-lm, hm, cm, om, vm = np.max(low) * 2, np.max(high) * 2, np.max(close) * 2, np.max(opn) * 2, np.max(vol) * 2
+f = 200
 x = [np.array(
-    list(low[i:i + 30] / lm) + list(high[i:i + 30] / hm) + list(close[i:i + 30] / cm) + list(
-        opn[i:i + 30] / om) + list(vol[i:i + 30] / vm)) for i in range(data.shape[0] - 37)]
+    list((low[i:i + input_days] - opn[i]) / f) + list((high[i:i + input_days] - opn[i]) / f) +
+    list((close[i:i + input_days] - opn[i]) / f) + list((opn[i:i + input_days] - opn[i]) / f) +
+    list(np.power(vol[i:i + input_days], 1 / 2) / f))
+    for i in range(data.shape[0] - input_days - output_days)]
 y = [np.array(
-    list(low[i:i + 7] / lm) + list(high[i:i + 7] / hm) + list(close[i:i + 7] / cm) + list(opn[i:i + 7] / om) + list(
-        vol[i:i + 7] / vm)) for i in range(30, data.shape[0] - 7)]
-test, train = tts(x, y, 1 / 6)
+    list((low[i:i + output_days] - opn[i - input_days]) / f) + list((high[i:i + output_days] - opn[i - input_days]) / f)
+    + list((close[i:i + output_days] - opn[i - input_days]) / f) +
+    list((opn[i:i + output_days] - opn[i - input_days]) / f) + list(np.power(vol[i:i + output_days], 1 / 2) / f))
+    for i in range(input_days, data.shape[0] - output_days)]
+np.cuda.Stream.null.synchronize()
+test, train = tts(x, y, amount_of_test_data)
+x = y = None
 
 # parameters
-arch = [5 * 30, 100, 70, 50, 5 * 7]
-shp(1, 10, 0)
+arch = [5 * input_days, 5000, 3000, 1000, 5 * output_days]
+shp(1e-5, 1, 0)
 
 # nw creation
 nw = new_nw(arch)
 
 
 # main part
-def iterate(mdl: NeuronWeb):
+def iterate(mdl: NeuronWeb, dt=None):
+    if dt is None:
+        dt = test
     c = 0
-    for tx, ty in test:
-        c += nweb.cost_function(ty, mdl.iterate(tx))
-    return c / len(test)
+    for tx, ty in dt:
+        c += float(np.sum(nweb.cost_function(ty, mdl.iterate(tx)[0]))) / len(ty)
+    np.cuda.Stream.null.synchronize()
+    return c / len(dt)
 
 
 def fit(mdl: NeuronWeb, epochs: int):
@@ -60,3 +84,21 @@ def fit(mdl: NeuronWeb, epochs: int):
         shuffle(train)
         for i in range(0, len(train), nweb.n):
             mdl.run(train[i:i + nweb.n], len(train))
+        np.cuda.Stream.null.synchronize()
+        global costs_to_show
+        costs_to_show += [iterate(mdl)]
+        print(costs_to_show[-1])
+
+
+def test_res(a: int):
+    return np.abs((test[a][1] - nw.iterate(test[a][0])[0]) / test[a][1])
+
+
+def show(yh, xl=0, xh=len(costs_to_show), yl=0):
+    plt.ylim(yl, yh)
+    plt.xlim(xl, xh)
+    plt.plot(costs_to_show)
+    plt.show()
+
+
+costs_to_show.append(iterate(nw))
--- code/nweb.py
+++ code/nweb.py
@@ -1,5 +1,6 @@
-import numpy as np
-from numpy import ndarray
+import cupy as np
+from cupy import ndarray
+import os
 
 # скорость обучения
 e = 0.25
@@ -10,9 +11,11 @@ lmb = 0.1
 
 
 def set_hyper_param(ee: float, nn: int, l: float):
-    global e, n, lmb
+    global e
     e = ee
+    global n
     n = nn
+    global lmb
     lmb = l
 
 
@@ -22,6 +25,45 @@ class NeuronWeb:
         self.a = a
         self.weights = [np.random.randn(a[i + 1], a[i]) / np.sqrt(a[i]) for i in range(len(a) - 1)]
         self.biases = [np.random.randn(i) for i in a[1:]]
+
+    def save(self, path, nw_name):
+        try:
+            os.mkdir(f'{path}/{nw_name}')
+            np.save(f'{path}/{nw_name}/arch.npy', np.array(self.a))
+            np.save(f'{path}/{nw_name}/params.npy', np.array([e, float(n), lmb]))
+            for i in range(len(self.weights)):
+                np.save(f'{path}/{nw_name}/weights_{i}.npy', self.weights[i])
+            for i in range(len(self.biases)):
+                np.save(f'{path}/{nw_name}/biases_{i}.npy', self.biases[i])
+        except FileExistsError:
+            inp = input('Do you really wanna rewrite old nw?(y/n)').lower()
+            if inp == 'y':
+                np.save(f'{path}/{nw_name}/arch.npy', np.array(self.a))
+                np.save(f'{path}/{nw_name}/params.npy', np.array([e, float(n), lmb]))
+                for i in range(len(self.weights)):
+                    np.save(f'{path}/{nw_name}/weights_{i}.npy', self.weights[i])
+                for i in range(len(self.biases)):
+                    np.save(f'{path}/{nw_name}/biases_{i}.npy', self.biases[i])
+            elif inp != 'n':
+                print('Can you just choose valid option?!')
+        except FileNotFoundError:
+            print('Error!!!\nWrong path!!!')
+
+    @staticmethod
+    def load(path, nw_name):
+        try:
+            nw = NeuronWeb([])
+            nw.a = np.load(f'{path}/{nw_name}/arch.npy').tolist()
+            params = np.load(f'{path}/{nw_name}/params.npy')
+            set_hyper_param(float(params[0]), int(params[1]), float(params[2]))
+            nw.weights = []
+            nw.biases = []
+            for i in range(len(nw.a) - 1):
+                nw.weights.append(np.load(f'{path}/{nw_name}/weights_{i}.npy'))
+                nw.biases.append(np.load(f'{path}/{nw_name}/biases_{i}.npy'))
+            return nw
+        except FileNotFoundError:
+            print('Error!!!\nWrong path!!!')
 
     def run(self, mini_batch: list[tuple[ndarray, ndarray]], size: int):
         db = [np.zeros(b.shape) for b in self.biases]
@@ -36,12 +78,13 @@ class NeuronWeb:
             self.weights[i] = (1 - lmb * e / size) * self.weights[i] - (e / n) * dw[i]
         for i in range(len(db)):
             self.biases[i] -= (e / n) * db[i]
+        np.cuda.Stream.null.synchronize()
 
     def iterate(self, data: ndarray):
         acts = [np.zeros(i) for i in self.a]
         acts[0] = data
         for l in range(len(self.biases)):
-            data = sig(np.dot(self.weights[l], np.transpose(data)) + self.biases[l])
+            data = tanh(np.dot(self.weights[l], np.transpose(data)) + self.biases[l])
             acts[l + 1] = data
         return data, acts
 
@@ -50,9 +93,10 @@ class NeuronWeb:
         dw = [np.zeros(w.shape) for w in self.weights]
         i = cost_derivative(acts[-1], i)
         for l in range(1, len(self.biases) + 1):
+            np.cuda.Stream.null.synchronize()
             db[-l] = i
             dw[-l] = np.dot(np.reshape(i, (len(i), 1)), np.reshape(acts[-l - 1], (1, len(acts[-l - 1]))))
-            i = np.dot(np.transpose(self.weights[-l]), i) * sig_derivative(acts[-l - 1])
+            i = np.dot(np.transpose(self.weights[-l]), i) * tanh_derivative(acts[-l - 1])
         return dw, db
 
 
@@ -62,18 +106,20 @@ class NeuronWeb:
 #
 # binary entrophy
 # c=-(y*ln(sig(z))+(1-y)ln(1-sig(z)))
-
+#
+# square function actually
 def cost_function(y: ndarray, a: ndarray):
-    return -(y * np.log(a) + (1 - y) * np.log(1 - a))
+    return (a - y) ** 2
 
 
 def cost_derivative(a: ndarray, y: ndarray):
-    return a - y
+    return 2 * (a - y) * tanh_derivative(a)
 
 
-def sig(z: ndarray):
-    return 1 / (1 + np.exp(-z))
+# tanh actually
+def tanh(z: ndarray):
+    return np.tanh(z)
 
 
-def sig_derivative(a: ndarray):
-    return a * (1 - a)
+def tanh_derivative(a: ndarray):
+    return 1 - a * a
